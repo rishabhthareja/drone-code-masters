20c8d5e28550d60942292b34ee2eb95ad85adfe1
rishabhthareja <rishabhthareja.2007@gmail.com>
2023-07-15 14:54:40 +0530

    added time delay to avoid faliure of commands

--- rotate_360.py
+++ rotate_360.py
@@ -25,14 +25,17 @@ battery_percentage = drone.get_battery()
 
 
 # Check if the battery percentage is above 80
-if battery_percentage > 80:
+if battery_percentage > 30:
     print("The battery percentage is: ", battery_percentage)
     # Take off the drone
     drone.takeoff()
     print("Drone is taking off...")
+    # The delay of 5 seconds is added to avoid any
+    # failed communication between laptop/PC and tello drone
+    sleep(5)
 
     # Rotate the drone 360 degrees
-    drone.rotate_counter_clockwise(360)
+    drone.rotate_clockwise(360)
     print("Drone is rotating 360 degrees...")
 
     # Hover for 2 seconds
